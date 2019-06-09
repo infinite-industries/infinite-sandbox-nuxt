6baa1f4b94adc6de92931d975ff2bb89c224183b
Chris Wininger <cwininger@sonatype.com>
2019-06-09 14:04:21 -0400

    remove logic from Callback.vue

--- web-portal/pages/Callback.vue
+++ web-portal/pages/Callback.vue
@@ -1,22 +1,5 @@
 <template>
-<div></div>
+  <div class="container d-flex align-items-center justify-content-center flex-column">
+   ...authorizing
+  </div>
 </template>
-<script>
-
-  function getParamByName (name) {
-    let match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash)
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
-  }
-
-  export default {
-    mounted () {
-      this.$nextTick(() => {
-        let accessToken = getParamByName('access_token')
-        let idToken = getParamByName('id_token')
-        this.$auth.$storage.setUniversal('access_token', accessToken, true)
-        this.$auth.$storage.setUniversal('id_token', idToken, true)
-        this.$router.replace('/')
-      })
-    }
-  }
-</script>
